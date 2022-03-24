27be7aa6633fde0f855621d8765931dfb835ca17
ashrafbd1496 <asraf1496@gmail.com>
2022-03-24 17:47:17 +0600

    blog post title, continue reading button and pagination done

--- index.php
+++ index.php
@@ -11,7 +11,32 @@ get_header(); ?>
       </div>
     </div>  
   </div>
+   <div class="container container--narrow page-section">
+    <?php 
+      while (have_posts()) {
+        the_post();?>
 
+        <div class="post-item">
+          <h3 class="headline headline--small-plus headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
+        </div>
+
+        <div class="metabox">
+         <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+        </div>
+
+        <div class="generic-content">
+          <?php echo the_excerpt(); ?>
+          <p><a class="btn btn--small btn--blue" href="<?php the_permalink(); ?>">Continue Reading &raquo;</a></p>
+        </div>
+
+
+
+     <?php } 
+
+     echo paginate_links();
+     ?>
+
+   </div>
 
 
 <?php get_footer();
