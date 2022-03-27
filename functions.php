1c58b6b5163505d181bf5066093cbe45216609e3
ashrafbd1496 <asraf1496@gmail.com>
2022-03-27 18:08:35 +0600

    created a custom filed Related Programs

--- functions.php
+++ functions.php
@@ -12,7 +12,6 @@ wp_enqueue_style('funiversity-main-stylesheet',get_stylesheet_uri());
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
-  //wp_enqueue_script('bootstrap-bundle-js',get_theme_file_uri('/bootstrap.bundle.min.js'),['jquery'],'default',true);
 }
 
 add_action('wp_enqueue_scripts','funiversity_files');
