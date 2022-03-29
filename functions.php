bb21e1086e7f54f50ae2bc275f9704d22e3d048d
ashrafbd1496 <asraf1496@gmail.com>
2022-03-29 19:47:21 +0600

    created pageBanner function

--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php 
 
+//pageBnner function to display banner image
+function pageBanner(){ ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php
+      $bannerImage = get_field('page_banner_background_image');
+      echo $bannerImage['sizes']['pageBanner'];?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <div class="page-banner__intro">
+        <p><?php the_field('page_banner_subtitle'); ?></p>
+      </div>
+    </div>  
+  </div>
+
+<?php } 
+
 function funiversity_files (){
 
 //css
