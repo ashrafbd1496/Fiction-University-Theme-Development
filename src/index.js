ea009a492930a2b635360d0f46766c9d5733c6fe
ashrafbd1496 <asraf1496@gmail.com>
2022-04-06 21:40:20 +0600

    created search class

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import "../css/style.scss"
 // Our modules / classes
 import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
+import GoogleMap from "./modules/GoogleMap"
+import Search from "./modules/Search"
 
 // Instantiate a new object using our modules/classes
 var mobileMenu = new MobileMenu()
 var heroSlider = new HeroSlider()
+var heroSlider = new GoogleMap()
+var MagicalSearch = new Search()
 
+ 
