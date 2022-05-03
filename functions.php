d2a5880d31e11f3a60b0a4ec2bce31f60bc28498
ashrafbd1496 <asraf1496@gmail.com>
2022-05-03 10:39:54 +0600

    Admin bar had hide for subscriber

--- functions.php
+++ functions.php
@@ -122,3 +122,10 @@ function redirectSubstoFrntend(){
       exit;
     }
   }
+  add_action('wp_loaded','noSubsAdminBar');
+    function noSubsAdminBar(){
+    $ourCurrentUser = wp_get_current_user();
+      if(count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0]== 'subscriber'){
+      show_admin_bar(false);
+    }
+  }
