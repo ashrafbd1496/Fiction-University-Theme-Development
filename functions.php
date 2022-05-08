9a251677c451edfc570a657e8d8945451307e2ac
ashrafbd1496 <asraf1496@gmail.com>
2022-05-08 11:38:12 +0600

    made the note post private from serverside using action_filter wp_insert_post_data

--- functions.php
+++ functions.php
@@ -150,4 +150,11 @@ function redirectSubstoFrntend(){
   function ourLoginTitle(){
     return get_bloginfo('name');
   }
-  
+// Force note post to be private
+add_filter('wp_insert_post_data','makeNotePrivate');
+function makeNotePrivate($data){
+  if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
+    $data['post_status'] = 'private';
+  }
+  return $data;
+}
