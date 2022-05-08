421624f53926199e7ec75d90afde62cdf2b65874
ashrafbd1496 <asraf1496@gmail.com>
2022-05-08 19:46:21 +0600

    made the note limit 4 for subscriber role

--- functions.php
+++ functions.php
@@ -153,6 +153,13 @@ function redirectSubstoFrntend(){
 // Force note post to be private
 add_filter('wp_insert_post_data','makeNotePrivate');
 function makeNotePrivate($data){
+  if ($data['post_type'] == 'note') {
+   if (count_user_posts(get_current_user_id(), 'note') > 4) {
+     die('You have reached your note limit');
+   }
+   $data['post_content'] = sanitize_textarea_field( $data['post_content'] );
+   $data['post_title'] = sanitize_textarea_field( $data['post_title'] );
+  }
   if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
     $data['post_status'] = 'private';
   }
