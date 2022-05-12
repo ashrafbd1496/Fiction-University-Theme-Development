752d6593fc19edb6be4e2d3c6073d30790d8a27b
ashrafbd1496 <asraf1496@gmail.com>
2022-05-12 09:12:08 +0600

    created new query for like button

--- single-professor.php
+++ single-professor.php
@@ -1,21 +1,50 @@
 <?php get_header();
-
 while (have_posts()) {
 	the_post();
 	pageBanner();
-
 	?>
-
-	
-
     <div class="container container--narrow page-section">
-    	
-
 		<!--<h2><?php //the_title(); ?></h2>-->
 		<div class="generic-content">
 			<div class="row group">
-				<div class="one-third"><?php the_post_thumbnail('professorPortrait');?></div>
-				<div class="two-third"><?php the_content(); ?></div>
+				<div class="one-third">
+					<?php the_post_thumbnail('professorPortrait');?>
+				</div>
+				<div class="two-third">
+					<?php 
+						$likeCount = new WP_Query([
+							'post_type' => 'like',
+							'meta_query' =>[
+								[
+									'key'=> 'liked_professor_id',
+									'compare'=> '=',
+									'value'=> get_the_ID(),
+								]
+							],
+						]);
+					$existStatus = 'no';
+						$existQuery = new WP_Query([
+						'author' => get_current_user_id(),
+						'post_type' => 'like',
+						'meta_query' =>[
+							[
+								'key'=> 'liked_professor_id',
+								'compare'=> '=',
+								'value'=> get_the_ID(),
+							]
+						],
+					]);
+						if ($existQuery->found_posts) {
+							$existStatus = 'yes';
+						}
+					 ?>
+					<span class="like-box" data-exists = "<?php echo $existStatus; ?>">
+						<i class="fa fa-heart-o" aria-hidden ='true'></i>
+						<i class="fa fa-heart" aria-hidden ='true'></i>
+						<span class="like-count"><?php echo $likeCount->found_posts; ?></span>
+					</span>
+					<?php the_content(); ?>
+				</div>
 			</div>
 		</div>
 
@@ -28,8 +57,7 @@ while (have_posts()) {
 				foreach ($relatedPrograms as $program) { ?>
 					<li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
 				<?php }
-				echo '</ul>';
-				
+				echo '</ul>';				
 			}
 		 ?>
 	</div>
